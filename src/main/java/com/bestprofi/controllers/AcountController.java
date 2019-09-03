1273224da2bdd20ad5c22ab5dde2e47ac961233b
nursultansman <nursultansman1996@gmail.com>
2019-09-03 18:25:50 +0600

    start now task modified

--- src/main/java/com/bestprofi/controllers/AcountController.java
+++ src/main/java/com/bestprofi/controllers/AcountController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @Controller
 public class AcountController {
